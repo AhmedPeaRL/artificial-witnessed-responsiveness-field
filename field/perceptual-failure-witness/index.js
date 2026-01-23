d8896256efb6e7fe446f94ab3ccff3bfe00b46f7
AhmedPeaRL <89841973+AhmedPeaRL@users.noreply.github.com>
2026-01-24 00:19:18 +0200

    Update index.js

--- field/perceptual-failure-witness/index.js
+++ field/perceptual-failure-witness/index.js
@@ -1,11 +1,9 @@
 import { observeField } from "../shared-field.js";
 import { registerContradiction } from "../contradiction-registry/index.js";
 
-observeField((state) => {
-  if (state.motionEnergy > 0.9 && state.silence === true) {
-    registerContradiction(
-      "PerceptualFailureWitness",
-      "High motion energy during declared silence"
-    );
-  }
-});
+export function registerPerceptualFailure() {
+  registerContradiction(
+    "PerceptualFailureWitness",
+    "Perceptual instability detected"
+  );
+}
