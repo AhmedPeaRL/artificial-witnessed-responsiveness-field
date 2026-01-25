655d7bdfacbac1beb24efaeeb4910eb163b220d2
AhmedPeaRL <89841973+AhmedPeaRL@users.noreply.github.com>
2026-01-25 06:54:27 +0200

    Update and rename silence/silence.js to silence.js

---
+++ silence.js
@@ -0,0 +1,4 @@
+setTimeout(() => {
+  const input = document.getElementById("disturbance");
+  input.style.opacity = 0.1;
+}, 15000);
--- silence/silence.js
+++
@@ -1,6 +0,0 @@
-// silence/silence.js
-export function enforceSilence() {
-  Object.freeze(localStorage);
-  Object.freeze(sessionStorage);
-  console.log = () => {};
-}
