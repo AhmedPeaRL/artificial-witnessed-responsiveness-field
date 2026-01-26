7afb94a5c0cabbbef9f554bd4d3c0b8547d900c0
AhmedPeaRL <89841973+AhmedPeaRL@users.noreply.github.com>
2026-01-26 03:12:23 +0200

    Delete field/perceptual-failure-witness directory

--- field/perceptual-failure-witness/index.js
+++
@@ -1,9 +0,0 @@
-import { observeField } from "../shared-field.js";
-import { registerContradiction } from "../contradiction-registry/index.js";
-
-export function registerPerceptualFailure() {
-  registerContradiction(
-    "PerceptualFailureWitness",
-    "Perceptual instability detected"
-  );
-}
